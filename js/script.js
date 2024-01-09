511057dfb7b3f24044042f6775c47be09fb26a0f
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-09 18:11:25 +0100

    created function to write a user reverse word

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 // Chiedere all'utente una parola
 const userWord = prompt('Inserisci una parola').trim();
 console.log("Parola dell'utente:", userWord);
+
+// Funzione per scrivere la parola dell'utente al contrario
+/**
+ * Reverse a given text
+ * @param {string} text the text to be reverse
+ * @returns {string} the reversed text
+ */
+function reverseText(text) {
+    let reversedText = '';
+
+    for (let i = text.length - 1; i >= 0 ; i--) {
+        reversedText += text[i];
+    }
+
+    return reversedText;
+}
+
+const result = reverseText(userWord);
+console.log("Parola dell'utente al contrario:", result);
